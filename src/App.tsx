06f383b5eea3959c075d7cabdf7fa1862397436d
minjaeKim <smallkdb@gmail.com>
2022-10-11 12:27:06 +0900

    feat: add StarRating UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import StarRating from './components/StarRate';
 
 function App() {
-  return (
-    <div className="App">
-      example
-    </div>
-  );
+	return (
+		<div className="App">
+			<StarRating />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/StarRate/LeaveStarRateContainer.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ReactComponent as Star } from '../../assets/svg/star.svg';
+import useContext from './useWrappingContext';
+import StarRateContext from './StarRateContext';
+import styles from './StarRating.module.scss';
+
+export default function LeaveStarRateContainer({ rating }: { rating: number }) {
+	const { handleMouseEnter } = useContext(StarRateContext);
+	return (
+		<div className={styles.starRateContainer} onMouseEnter={handleMouseEnter}>
+			{[1, 2, 3, 4, 5].map((num) => (
+				<button key={num} type="button" className={styles.wrapper}>
+					<Star key={num} className={styles.wrapper__star} fill={rating >= num ? '#ff7f23' : '#eee'} />
+				</button>
+			))}
+		</div>
+	);
+}
